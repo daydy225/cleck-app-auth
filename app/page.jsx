348d2642dd179f10a53700d9e25d552089170d94
daydy225 <dabelydadi@gmail.com>
2023-09-25 08:23:33 +0000

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-5">Welcome</h1>
+      <p className="mb-5">
+        This is a demo site for Daydy App Development Nextjs & Clerk Tutorial.
+        Go ahead and sign up or sign in!
+      </p>
+    </>
+  )
+}
